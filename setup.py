8b3b52721e57b7f1e401c1c0599d540a7c45e60c
Ben Keller <bjkeller@uw.edu>
2020-10-07 23:21:17 +0000

    Add initial setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+readme = open('README.md').read()
+requirements_list = [pkg for pkg in open('requirements.txt').readlines]
+
+setup(
+    name='aquarium-opil',
+    version='0.0.1',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    install_requires=requirements_list,
+
+    author='Ben Keller',
+    author_email='bjkeller@uw.edu',
+    description='Package for exposing Aquarium workflows as OPIL',
+    license='MIT',
+    long_description=readme,
+    url='https://aquariumbio.bio',
+    project_urls={
+        'Source Code': 'https://github.com/aquariumbio/aquarium-opil'
+    }
+)
